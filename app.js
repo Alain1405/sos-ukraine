4b76f72a4fcf5d8c0f50e87da0f2f4691d8ee165
Alain Scialoja <>
2022-03-04 00:46:23 +0800

    added requests page

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config()
+const pug = require('pug');
+
 
 // Sequelize
 DATABASE_URL = process.env.DATABASE_URL
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(DATABASE_URL, {
-    dialectOptions: {
+dbOptions = {}
+if (process.env.ENV != 'DEV') {
+    dbOptions[dialectOptions] = {
         ssl: {
-            require: true,
+            require: process.env.ENV != 'DEV',
             rejectUnauthorized: false
         }
     }
-}) // Example for postgres
+}
+const sequelize = new Sequelize(DATABASE_URL, dbOptions) // Example for postgres
 
 class PickupRequest extends Model { }
 PickupRequest.init({
@@ -64,11 +68,24 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
 
+app.use(bodyParser.urlencoded({ extended: true }));
 // sendFile will go here
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/requests', function (req, res) {
+    PickupRequest.findAll().then((results) => {
+        results.forEach(element => {
+            element.map_link = "https://www.google.com/maps/search/?api=1&query=" + element.lat + "," + element.lng;
+            element.coordinates = "Lat:" + element.lat + "; Lng: " + element.lng;
+        });
+        console.log(results)
+        res.render('table', { title: 'Express', requests: results });
+
+    })
 });
 app.post('/submitLocation', function (req, res) {
     console.log(req.body);
